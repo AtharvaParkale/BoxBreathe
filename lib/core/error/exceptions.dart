98466b70125bdc3595acaf93ce5d9a5c0e2d6606
atharvaparkale <atharva.parkale@gmail.com>
2026-08-24 01:09:55 +0530

    :memo: add version code changes

--- lib/core/error/exceptions.dart
+++ lib/core/error/exceptions.dart
@@ -1,1 +1,12 @@
 class CacheException implements Exception {}
+
+class AuthException implements Exception {
+  final String message;
+  final String? code;
+  const AuthException([this.message = '', this.code]);
+}
+
+class ServerException implements Exception {
+  final String message;
+  const ServerException([this.message = '']);
+}
--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -11,3 +11,11 @@ abstract class Failure extends Equatable {
 class CacheFailure extends Failure {
   const CacheFailure([super.message]);
 }
+
+class AuthFailure extends Failure {
+  const AuthFailure([super.message]);
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure([super.message]);
+}
